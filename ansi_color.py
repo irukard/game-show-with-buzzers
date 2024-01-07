7543c5cf52cb98f507d29ea4ee192a5dd2293ee5
Krzysztof Daszuta <irukard@gmail.com>
2024-01-08 00:53:09 +0100

    Window app with buzzers.

---
+++ ansi_color.py
@@ -0,0 +1,18 @@
+class Color:
+    RED = "\033[31m"
+    GREEN = "\033[91m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    PURPLE = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    
+    RED_HI = "\033[91m"
+    GREEN_HI = "\033[92m"
+    YELLOW_HI = "\033[93m"
+    BLUE_HI = "\033[94m"
+    PURPLE_HI = "\033[95m"
+    CYAN_HI = "\033[96m"
+    WHITE_HI = "\033[97m"
+    
+    RESET = '\033[0m'
--- window_app.py
+++ window_app.py
@@ -1,5 +1,19 @@
 import tkinter as tk
 from tkinter import ttk
+from ansi_color import Color
+import pybuzzers
+import time
+import os
+
+# import winsound
+# Play wav file under windows
+# winsound.PlaySound("./audio/audio_clip.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
+
+# import pyautogui
+# Use multimedia keys to control media player: pyautogui.press("nexttrack")
+# "playpause", "stop", "prevtrack", "nexttrack", "volumemute", etc
+
+os.system("") # enable colors
 
 class Game(tk.Tk):
     def __init__(self, number_of_teams: int):
@@ -9,22 +23,47 @@ class Game(tk.Tk):
         self.frame_center = ttk.Frame(master=self)
         self.frame_center.pack(side="top", pady=(200,100))
         self.teams:Team = []
+        self.we_got_buzz = False
+
+        self.controlers = GameBuzzers()
+        self.controlers.buzzer_set.on_buzz(self.respond_to_buzz)
+        self.controlers.buzzer_set.start_listening()
         
         for i in range(number_of_teams):
             self.teams.append(Team(self, "Team " + str(i+1)))
-        
+
         self.frame_management = ttk.Frame(master=self)
+
         self.button_next_turn = ttk.Button(master=self.frame_management,
-                                           text="Next round",
+                                           text="▶ Try again",
+                                           command=self.round_again,
+                                           padding=7)
+        self.button_next_turn.pack(side="left", padx=15)
+
+        self.button_next_turn = ttk.Button(master=self.frame_management,
+                                           text="⏩ Next round",
                                            command=self.next_round,
                                            padding=7)
-        self.button_next_turn.pack()
+        self.button_next_turn.pack(side="left", padx=15)
+
         self.frame_management.pack(side="top")
     
     def next_round(self):
         for team in self.teams:
             team.unlock_team()
-       
+        self.controlers.buzzers_blink(3)
+        self.we_got_buzz = False
+
+    def round_again(self):
+        self.controlers.buzzers_blink(3)
+        self.we_got_buzz = False
+
+    def respond_to_buzz(self, _, id: int):
+        if self.teams[id].is_unlocked() and (self.we_got_buzz == False):
+            self.we_got_buzz = True
+            self.teams[id].mark_team()
+            self.controlers.buzzer_set.set_light(id, True)
+
 class Team():
     def __init__(self, window: Game, team_name: str):
         self.team_score = tk.IntVar()
@@ -93,22 +132,52 @@ class Team():
         self.label_team_score.configure(foreground="lightgray")
         self.button_lock.configure(text="Unlock")
         self.team_locked.set(True)
-        self.clear_marker()
-    
+
     def unlock_team(self):
         self.label_team_name.configure(foreground="black")
         self.label_team_score.configure(foreground="black")
         self.button_lock.configure(text="Lock")
         self.team_locked.set(False)
-        
+
     def mark_team(self):
-        self.label_team_name.configure(foreground="red")
+        self.label_team_name.configure(foreground="orange")
         self.label_team_score.configure(foreground="orange")
+
+    def is_unlocked(self):
+        if not self.team_locked.get():
+            return True
+        else:
+            return False
+
+class GameBuzzers():
+    def __init__(self):
+        try:
+            self.buzzer_set = pybuzzers.get_all_buzzers()[0]
+        except IndexError:
+            print(f"{Color.RED_HI}Error: No buzzers were detected{Color.RESET}")
+            print("Please connect receiver and run program again.")
+            quit()
+        else:
+            print(f"{Color.GREEN_HI}Buzzers detected{Color.RESET}")
+            self.buzzers_blink(2)
+            self.buzzers_sweep(2)
+
+    def buzzers_blink(self, times:int):
+        for _ in range(times):
+            self.buzzer_set.set_lights_on()
+            time.sleep(0.2)
+            self.buzzer_set.set_lights_off()
+            time.sleep(0.2)
+
+    def buzzers_sweep(self, times:int):
+        for _ in range(times):
+            for i in range(4):
+                self.buzzer_set.set_light(i, True)
+                time.sleep(0.1)
+                self.buzzer_set.set_light(i, False)
     
 def main():
     game = Game(4)
-    game.teams[0].mark_team()
-    
     game.mainloop()
 
 if __name__ == "__main__":
